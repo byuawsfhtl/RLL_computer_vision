028465777cb0cf34427be2276188390b178f2c89
haltosan <32181538+haltosan@users.noreply.github.com>
2023-03-06 11:03:26 -0700

    Add point reorganization

--- interpolate.py
+++ interpolate.py
@@ -1,17 +1,15 @@
 PAGE_BREAK_THRESH = 200
 CONNECTED_THRESH = 10
+APPROX_THRESH = 10
+ROW_LENGTH = 1000
 
-VOLUME_NUM = '88'
-IMAGE_NUM = '70'
+VOLUME_NUM = '85'
+IMAGE_NUM = '11'
 
 import os
 import json
 
-jsonName = 'record_image_vol_' + VOLUME_NUM + '_num_' + IMAGE_NUM + '.json'
 
-os.chdir(r'V:\FHSS-JoePriceResearch\papers\current\colorado_land_patents\data\tract books\row from full training')
-j = json.load(open(jsonName,'r'))
-shapes = j['shapes']
 
 '''              ===labelme points===
 order placed
@@ -34,6 +32,8 @@ place the points in the following order:
 joins then happen between shape[i] and shape[i+1]
  an exception when shape[i] is bottom of page 1 and shape[i+1] is top of page 2 
 '''
+
+shapesAdded = 0
 
 def pageBreak(a:[list], b:[list]) -> bool:
     return abs(a[0][1] - b[3][1]) > PAGE_BREAK_THRESH
@@ -62,14 +62,74 @@ def postProcess(shapes:[dict]) -> [dict]:
             out.append(i)
     return out
 
-shapesAdded = 0
+def addShapes():
+    jsonName = 'record_image_vol_' + VOLUME_NUM + '_num_' + IMAGE_NUM + '.json'
 
-newShapes = list()
-for i in range(len(shapes) - 1):
-    newShapes.append(shapes[i])
-    newShapes.append(connect(shapes[i], shapes[i+1]))
+    os.chdir(r'V:\FHSS-JoePriceResearch\papers\current\colorado_land_patents\data\tract books\row from full training')
+    j = json.load(open(jsonName,'r'))
+    shapes = j['shapes']
+    sortedShapes = list()
+    for shape in shapes:
+        points = shape['points']
+        newShape = pointsToShape(sortPoints(points))
+        sortedShapes.append(newShape)
+    shapes = sortedShapes
     
-newShapes.append(shapes[-1])
-j['shapes'] = postProcess(newShapes) 
-json.dump(j, open(jsonName,'w'))
-print(shapesAdded, 'shapes added')
+    newShapes = list()
+    for i in range(len(shapes) - 1):
+        newShapes.append(shapes[i])
+        newShapes.append(connect(shapes[i], shapes[i+1]))
+        
+    newShapes.append(shapes[-1])
+    j['shapes'] = postProcess(newShapes) 
+    json.dump(j, open(jsonName,'w'))
+    print(shapesAdded, 'shapes added')
+
+
+def approx(a:float, b:float)->bool:
+    return abs(a-b) < APPROX_THRESH
+
+def rowLength(a:float, b:float)->bool:
+    return abs(a-b) > ROW_LENGTH
+
+def makeLine(a:[int,int], b:[int,int])->str:
+    if(approx(a[0], b[0]) and not rowLength(a[0],b[0])):
+        if(a[1] < b[1]):
+            return 'Down'
+        return 'Up'
+    if(approx(a[1], b[1])):
+        if(a[0] < b[0]):
+            return 'Right'
+        return 'Left'
+    if(a[0] < b[0]):
+        if(a[1] < b[1]):
+            return 'Positive'
+        return 'Negative'
+    if(a[1] < b[1]):
+        return 'Negative'
+    return 'Positive'
+
+def makePattern(points:[list])->[str,str]:
+    return [makeLine(points[0], points[1]), makeLine(points[0], points[2])]
+
+def matchPattern(pattern:[str,str], points:[int,int]) -> list:
+    print(pattern)
+    options = {'Up'    : {'Negative':[1,4,3,2], 'Positive':[2,3,4,1]},
+               'Right' : {'Positive':[4,3,2,1], 'Negative':[1,2,3,4]},
+               'Down'  : {'Negative':[3,2,1,4], 'Positive':[4,1,2,3]},
+               'Left'  : {'Positive':[2,1,4,3], 'Negative':[3,4,1,2]}
+               }
+    order = options[pattern[0]][pattern[1]]
+    return [[points[i], order[i]] for i in range(4)]
+
+def sortPoints(points:[list])->[list]:
+    print(points)
+    newPoints = matchPattern(makePattern(points), points)
+    newPoints.sort(key = lambda i : i[1])
+    return [i[0] for i in newPoints]
+
+
+
+addShapes()
+
+
