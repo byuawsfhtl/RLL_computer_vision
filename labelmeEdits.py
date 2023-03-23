75a379df4df23f8a51acf22fa72996ad580960b9
haltosan <32181538+haltosan@users.noreply.github.com>
2023-03-23 14:15:03 -0600

    Add files via upload

--- labelmeEdits.py
+++ labelmeEdits.py
@@ -5,13 +5,13 @@ ROW_LENGTH = 1000
 
 X_GROUPING = 900
 
-VOLUME_NUM = '90'
-IMAGE_NUM = '94'
+VOLUME_NUM = '94'
+IMAGE_NUM = '52'
 
 ### ENDED ON 90 94
 
 jsonName = 'record_image_vol_' + VOLUME_NUM + '_num_' + IMAGE_NUM + '.json'
-workingDir = r'V:\FHSS-JoePriceResearch\papers\current\colorado_land_patents\data\tract books\predicted'
+workingDir = r'V:\papers\current\colorado_land_patents\data\tract books\predicted'
 
 import os
 import json
@@ -258,11 +258,8 @@ def smooth(j:dict, smoothFactor:float = 1.00, method:str='median', goalList=[[No
             goal = lambda acc: acc / lastLeft
 
         if method == 'goal':
-            print('goalList', goalList)
             leftGoal = goalList[pageI][0]  # [[leftPageLeftSide, leftPageRightSide], [rightPageLeftSide, rightPageRightSide]]
             rightGoal = goalList[pageI][1]
-            print('goals', leftGoal, rightGoal)
-            print('page', page, '\n')
         else:
             for i in page:
                 shape = shapes[i]
